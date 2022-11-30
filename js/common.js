5fcb0a29c9c5e6f1fbbe6d412e70102b1549d68e
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2022-11-30 12:13:07 +0900

    feat  : 글 상세 보기 추가

--- js/common.js
+++ js/common.js
@@ -1,11 +1,15 @@
 const members = await fetch("../json/members.json").then(toJSON);
 const categorys = await fetch("../json/categorys.json").then(toJSON);
 const posts = await fetch("../json/posts.json").then(toJSON);
+posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
 const rowLimit = 20;
 let currentRow = 0;
 
+const $galleryModal = document.querySelector(".gallery-modal");
 const $galleryContainer = document.querySelector(".gallery .item-container");
+const $memberFilter = document.querySelector(".member-filter");
+const $categoryFilter = document.querySelector(".category-filter");
 const services = {
     findMember(findMemberIndex) {
         return members.find(({ index }) => findMemberIndex === index);
@@ -48,7 +52,50 @@ function renderGalleryItem(gallery) {
             <p class="date">${gallery.date}</p>
         </div>
     `;
+    $galleryItem.addEventListener("click", function () {
+        console.log("1");
+        setModalContent(gallery);
+        modalOpen();
+    });
     $galleryContainer.append($galleryItem);
 }
 
+function setModalContent(gallery) {
+    const $galleryModal = document.querySelector(".gallery-modal");
+    const $title = $galleryModal.querySelector(".title");
+    const $description = $galleryModal.querySelector(".description");
+    const $categorys = $galleryModal.querySelector(".categorys");
+    const $user = $galleryModal.querySelector(".user>span");
+    const $date = $galleryModal.querySelector(".date");
+
+    const member = services.findMember(gallery.memberIndex);
+    const categorys = gallery.categorys.map(services.findCategory);
+    $title.textContent = gallery.title;
+    $description.textContent = gallery.contents;
+    $user.textContent = member.name;
+    $date.textContent = gallery.date;
+    $categorys.innerHTML = categorys
+        .map(
+            ({ color, name }) =>
+                `<span class="category" style="background:${color}">#${name}</span>`
+        )
+        .join("");
+}
+function modalOpen() {
+    const $galleryModal = document.querySelector(".gallery-modal");
+    $galleryModal.classList.add("open");
+}
+function modalClose() {
+    const $galleryModal = document.querySelector(".gallery-modal");
+    $galleryModal.classList.remove("open");
+}
+
+$galleryModal.addEventListener("click", function (e) {
+    modalClose();
+});
+
+const c = $galleryModal.querySelector(".modal-content");
+c.addEventListener("click", function (e) {
+    e.stopPropagation();
+});
 renderGallerys();
